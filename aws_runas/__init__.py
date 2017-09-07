da409ef6758116b0c36ffbc745d1a6c3b91aa2b2
Mike Morris <michael.m.morris@pearson.com>
2017-09-07 12:38:20 -0500

    version bump to release version 0.1.5

--- aws_runas/__init__.py
+++ aws_runas/__init__.py
@@ -8,7 +8,7 @@ import botocore.session
 
 from botocore.exceptions import ProfileNotFound
 
-__VERSION__ = '0.1.4'
+__VERSION__ = '0.1.5'
 
 # cribbed from the awscli assumerole.py customization module
 class JSONFileCache(object):
