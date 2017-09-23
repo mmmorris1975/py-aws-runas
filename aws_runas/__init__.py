161ccdb316e1e2dd79cdd76f70be3988f39bd7d4
Mike Morris <michael.m.morris@pearson.com>
2017-09-23 11:06:42 -0500

    implement '-s' option to dispaly eval-able() session token credentials instead of assume role credentials

--- aws_runas/__init__.py
+++ aws_runas/__init__.py
@@ -1,4 +1,4 @@
-import os, time
+import os, sys, time
 import logging
 import boto3
 import argparse
@@ -13,12 +13,14 @@ except ImportError:
   # Python 2
   from ConfigParser import ConfigParser
 
-__VERSION__ = '0.2.0-beta2'
+__VERSION__ = '0.2.0-betaF'
 
 def parse_cmdline():
   p = argparse.ArgumentParser(description='Create an environment for interacting with the AWS API using an assumed role')
   p.add_argument('-l', '--list-roles', help='list role ARNs you are able to assume', action='store_true')
   p.add_argument('-m', '--list-mfa', help='list the ARN of the MFA device associated with the account', action='store_true')
+  p.add_argument('-e', '--expiration', help='Show token expiration time', action='store_true')
+  p.add_argument('-s', '--session', help='print eval()-able session token info', action='store_true')
   p.add_argument('-v', '--verbose', help='print verbose/debug messages', action='store_const', const=logging.DEBUG, default=logging.INFO)
   p.add_argument('-V', '--version', help='print program version and exit', action='store_true')
   p.add_argument('profile', nargs='?', help='name of profile')
@@ -94,17 +96,48 @@ def parse_policy_doc(doc):
 
 def parse_aws_config(profile):
   cfg_file = os.getenv('AWS_CONFIG_FILE', os.path.expanduser(os.path.join('~', '.aws', 'config')))
-  cfg_profile = "profile " + profile
   logging.debug("CONFIG FILE: %s", cfg_file)
+
+  if not profile:
+    profile = os.getenv('AWS_PROFILE', os.getenv('AWS_DEFAULT_PROFILE'))
+    if not profile:
+      profile = 'default'
+
+  if profile == 'default':
+    cfg_profile = 'default'
+  else:
+    cfg_profile = "profile " + profile
 
   p = ConfigParser()
   p.read(cfg_file)
 
-  src_profile = p.get(cfg_profile, 'source_profile')
-  role_arn = p.get(cfg_profile, 'role_arn')
-  mfa_serial = p.get(src_profile, 'mfa_serial')
+  try:
+    src_profile = p.get(cfg_profile, 'source_profile')
+  except:
+    src_profile = cfg_profile
 
-  return (src_profile, role_arn, mfa_serial)
+  try:
+    role_arn = p.get(cfg_profile, 'role_arn')
+  except:
+    role_arn = None
+
+  try:
+    mfa_serial = p.get(src_profile, 'mfa_serial')
+  except:
+    mfa_serial = None
+
+  return (src_profile.replace('profile ', '', 1), role_arn, mfa_serial)
+
+def print_creds(creds):
+  exp_cmd = 'export'
+
+  if os.name == 'nt':
+    exp_cmd = 'set'
+
+  print("%s %s=%s" % (exp_cmd, 'AWS_ACCESS_KEY_ID', creds['AccessKeyId']))
+  print("%s %s=%s" % (exp_cmd, 'AWS_SECRET_ACCESS_KEY', creds['SecretAccessKey']))
+  print("%s %s=%s" % (exp_cmd, 'AWS_SESSION_TOKEN', creds['SessionToken']))
+  print("%s %s=%s" % (exp_cmd, 'AWS_SECURITY_TOKEN', creds['SessionToken']))
 
 def main():
   args = parse_cmdline()
@@ -163,6 +196,15 @@ def main():
     res = p.get_credentials()
 
     creds = res['Credentials']
+    if args.session:
+      print_creds(creds)
+      sys.exit(0)
+
+    if args.expiration:
+      # TODO print credential expiration
+      logging.info("Would print cred expiration here")
+      sys.exit(0)
+
     tok_ses = boto3.Session( aws_access_key_id=creds['AccessKeyId'], aws_secret_access_key=creds['SecretAccessKey'],
         aws_session_token=creds['SessionToken'])
     tok_sts = tok_ses.client('sts')
@@ -182,11 +224,7 @@ def main():
     if len(args.cmd) == 0:
       # profile name only, output the keys and tokens as env vars
       logging.debug("no command detected, outputting eval()-able role credentials")
-      for i in ('AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY', 'AWS_SESSION_TOKEN', 'AWS_SECURITY_TOKEN'):
-        if os.name == 'nt':
-          print("set %s='%s'" % (i, os.getenv(i)))
-        else:
-          print("export %s='%s'" % (i, os.getenv(i)))
+      print_creds(ar_creds)
     else:
       # role name and command to run
       logging.debug("detected command, will run command using assumed role")
